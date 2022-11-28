40f1dbb84d7f35370a9745b4aa6320c792e2ca4a
Rafael Neves Gomila <rafael.gomila@hotmail.com>
2022-11-28 16:48:49 -0300

    Seeding

---
+++ desafios/client/src/main/resources/import.sql
@@ -0,0 +1,10 @@
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Sandra', '15039402918', 2021.00, TIMESTAMP WITH TIME ZONE '1995-03-03T21:50:07.12345Z', 5);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Carlos', '17593019572', 20185.00, TIMESTAMP WITH TIME ZONE '1942-17-01T20:50:07.12345Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Bruna', '19385748104', 17492.00, TIMESTAMP WITH TIME ZONE '1049-07-13T20:50:07.12345Z', 3);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Roberto', '04917482953', 102.00, TIMESTAMP WITH TIME ZONE '1029-07-13T20:50:07.12345Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Fernanda', '58291765301', 10309.00, TIMESTAMP WITH TIME ZONE '1942-07-13T20:50:07.12345Z', 7);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Francisco', '29305710495', 6350.00, TIMESTAMP WITH TIME ZONE '1903-07-13T20:50:07.12345Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Amanda', '75920194728', 25500.00, TIMESTAMP WITH TIME ZONE '2002-06-13T20:50:07.12345Z', 6);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Diego', '105947285942', 1500.00, TIMESTAMP WITH TIME ZONE '2010-07-13T20:50:07.12345Z', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Beatriz', '102948592015', 2500.00, TIMESTAMP WITH TIME ZONE '1912-03-13T20:50:07.12345Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Rafael', '203947195029', 7500.00, TIMESTAMP WITH TIME ZONE '1998-02-27T20:50:07.12345Z', 10);
