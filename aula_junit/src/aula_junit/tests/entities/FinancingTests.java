090fd7b371a4b57e2ed5ebad07522d7c0f40f006
Rafael Neves Gomila <rafael.gomila@hotmail.com>
2022-12-05 04:54:17 -0300

    test activity for setMonth

--- aula_junit/src/aula_junit/tests/entities/FinancingTests.java
+++ aula_junit/src/aula_junit/tests/entities/FinancingTests.java
@@ -7,6 +7,26 @@ import org.junit.jupiter.api.Test;
 public class FinancingTests {
 
     @Test
+    public void setMonthsShouldChangeMonthOfFinancingWhenRequestChangeMonth() {
+        Financing financing = new Financing(1l, 200000.00, 4000.00, 80);
+
+        financing.setMonths(160);
+
+        Assertions.assertEquals(160, financing.getMonths());
+    }
+
+    @Test
+    public void setMonthsShouldThorwExceptionWhenRequestChangeMonthNotValid() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            Financing financing = new Financing(1l, 200000.00, 4000.00, 80);
+
+            financing.setMonths(60);
+
+            Assertions.assertEquals(60, financing.getMonths());
+        });
+    }
+
+    @Test
     public void entryShouldInitialTotalAmount() {
         Financing financing = new Financing(1l, 200000.00, 4000.00, 80);
 
