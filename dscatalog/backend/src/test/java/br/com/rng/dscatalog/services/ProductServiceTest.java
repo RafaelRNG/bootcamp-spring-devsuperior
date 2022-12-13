450a15a9b5f3943f1589378e1c0a368e8f0a0a09
Rafael Neves Gomila <rafael.gomila@hotmail.com>
2022-12-12 23:49:57 -0300

    Test deleteShouldThrowDatabaseExceptionWhenDependentId

--- dscatalog/backend/src/test/java/br/com/rng/dscatalog/services/ProductServiceTest.java
+++ dscatalog/backend/src/test/java/br/com/rng/dscatalog/services/ProductServiceTest.java
@@ -1,6 +1,7 @@
 package br.com.rng.dscatalog.services;
 
 import br.com.rng.dscatalog.repositories.ProductRepository;
+import br.com.rng.dscatalog.services.exceptions.DatabaseException;
 import br.com.rng.dscatalog.services.exceptions.ResourceNotFoundException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -9,6 +10,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -23,15 +25,19 @@ public class ProductServiceTest {
 
     private long existingId;
     private long nonExistingId;
+    private long dependentId;
 
     @BeforeEach
     void setUp() throws Exception {
         existingId = 1L;
         nonExistingId = 1000L;
+        dependentId = 4L;
 
         Mockito.doNothing().when(productRepository).deleteById(existingId);
 
         Mockito.doThrow(EmptyResultDataAccessException.class).when(productRepository).deleteById(nonExistingId);
+
+        Mockito.doThrow(DataIntegrityViolationException.class).when(productRepository).deleteById(dependentId);
     }
 
     @Test
@@ -50,5 +56,14 @@ public class ProductServiceTest {
         });
 
         Mockito.verify(productRepository, Mockito.times(1)).deleteById(nonExistingId);
+    }
+
+    @Test
+    public void deleteShouldThrowDatabaseExceptionWhenDependentId() {
+        Assertions.assertThrows(DatabaseException.class, () -> {
+            productService.delete(dependentId);
+        });
+
+        Mockito.verify(productRepository, Mockito.times(1)).deleteById(dependentId);
     }
 }
