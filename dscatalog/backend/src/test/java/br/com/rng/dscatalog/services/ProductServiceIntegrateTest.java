9b1239a498740525320e0be0389bdabb71907793
Rafael Neves Gomila <rafael.gomila@hotmail.com>
2022-12-14 16:10:42 -0300

    Tests deleteById ProductIntegrateTest

---
+++ dscatalog/backend/src/test/java/br/com/rng/dscatalog/services/ProductServiceIntegrateTest.java
@@ -0,0 +1,45 @@
+package br.com.rng.dscatalog.services;
+
+import br.com.rng.dscatalog.repositories.ProductRepository;
+import br.com.rng.dscatalog.services.exceptions.ResourceNotFoundException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ProductServiceIntegrateTest {
+
+    @Autowired
+    private ProductService productService;
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    private long existingId;
+    private long nonExistingId;
+    private long countTotalProducts;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        existingId = 1L;
+        nonExistingId = 1000L;
+        countTotalProducts = 25L;
+    }
+
+    @Test
+    public void deleteShouldDeleteResourceWhenIdExists() {
+
+        productService.delete(existingId);
+
+        Assertions.assertEquals(countTotalProducts - 1, productRepository.count());
+    }
+
+    @Test
+    public void deleteShouldThrowResourceNotFoundExceptionWhenIdDoesNotExists() {
+        Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+            productService.delete(nonExistingId);
+        });
+    }
+}
