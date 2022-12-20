ce90a88e7c5ac4b0988f36e20c90a337a7460669
Rafael Neves Gomila <rafael.gomila@hotmail.com>
2022-12-20 16:40:36 -0300

    Custom validation response

--- dscatalog/backend/src/main/java/br/com/rng/dscatalog/resources/exceptions/ResourceExceptionHandler.java
+++ dscatalog/backend/src/main/java/br/com/rng/dscatalog/resources/exceptions/ResourceExceptionHandler.java
@@ -5,6 +5,8 @@ import br.com.rng.dscatalog.services.exceptions.ResourceNotFoundException;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -15,8 +17,7 @@ import java.time.Instant;
 public class ResourceExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<StandardError> entityNotFound(ResourceNotFoundException entityNotFoundException,
-                                                        HttpServletRequest httpServletRequest) {
+    public ResponseEntity<StandardError> entityNotFound(ResourceNotFoundException entityNotFoundException, HttpServletRequest httpServletRequest) {
 
         Integer status = HttpStatus.NOT_FOUND.value();
 
@@ -45,4 +46,21 @@ public class ResourceExceptionHandler {
         return ResponseEntity.status(status).body(standardError);
     }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ValidationError> validation(MethodArgumentNotValidException notValidException, HttpServletRequest httpServletRequest) {
+        Integer status = HttpStatus.UNPROCESSABLE_ENTITY.value();
+
+        ValidationError validationError = new ValidationError();
+        validationError.setTimestamp(Instant.now());
+        validationError.setStatus(status);
+        validationError.setError("Validation exception");
+        validationError.setMessage("Fields Error");
+        validationError.setPath(httpServletRequest.getRequestURI());
+
+        for(FieldError fieldError: notValidException.getBindingResult().getFieldErrors()) {
+            validationError.addError(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+
+        return ResponseEntity.status(status).body(validationError);
+    }
 }
