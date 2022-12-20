5b836e6c9da880d4fa21d153f3f51627209b5145
Rafael Neves Gomila <rafael.gomila@hotmail.com>
2022-12-20 17:55:32 -0300

    Custom constraintValidator

--- dscatalog/backend/src/main/java/br/com/rng/dscatalog/dtos/UserInsertDTO.java
+++ dscatalog/backend/src/main/java/br/com/rng/dscatalog/dtos/UserInsertDTO.java
@@ -1,12 +1,17 @@
 package br.com.rng.dscatalog.dtos;
 
+import br.com.rng.dscatalog.services.validations.UserInsertValid;
+
+@UserInsertValid
 public class UserInsertDTO extends UserDTO {
 
     private static final long serialVersionUID = 1L;
 
     private String password;
 
-    public UserInsertDTO() {}
+    public UserInsertDTO() {
+        super();
+    }
 
     public String getPassword() {
         return password;
---
+++ dscatalog/backend/src/main/java/br/com/rng/dscatalog/services/validations/UserInsertValidator.java
@@ -0,0 +1,40 @@
+package br.com.rng.dscatalog.services.validations;
+
+import br.com.rng.dscatalog.dtos.UserInsertDTO;
+import br.com.rng.dscatalog.entities.User;
+import br.com.rng.dscatalog.repositories.UserRepository;
+import br.com.rng.dscatalog.resources.exceptions.FieldMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserInsertValidator implements ConstraintValidator<UserInsertValid, UserInsertDTO> {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void initialize(UserInsertValid ann) {}
+
+    @Override
+    public boolean isValid(UserInsertDTO dto, ConstraintValidatorContext context) {
+
+        List<FieldMessage> list = new ArrayList<>();
+
+        User user = userRepository.findByEmail(dto.getEmail());
+
+        if(user != null) {
+            list.add(new FieldMessage("email", "E-mail já existe"));
+        }
+
+        for (FieldMessage e : list) {
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+                    .addConstraintViolation();
+        }
+        return list.isEmpty();
+    }
+}
