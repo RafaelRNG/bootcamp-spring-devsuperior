68917f690ee5ba9686cdcb5be81f37544fe11e53
Rafael Neves Gomila <rafael.gomila@hotmail.com>
2023-01-09 23:04:06 -0300

    Notification

--- dslearn/backend/src/main/resources/import.sql
+++ dslearn/backend/src/main/resources/import.sql
@@ -6,7 +6,6 @@ INSERT INTO tb_role (authority) VALUES ('ROLE_STUDENT');
 INSERT INTO tb_role (authority) VALUES ('ROLE_INSTRUCTOR');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
 
-
 INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
@@ -18,3 +17,5 @@ INSERT INTO tb_course(name, img_uri, img_gray_uri) VALUES ('Bootscamp HTML', 'ht
 
 INSERT INTO tb_offer(edition, start_moment, end_moment, course_id) VALUES('1.0', TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07.12345Z', TIMESTAMP WITH TIME ZONE '2020-09-15T09:50:07.12345Z', 1);
 INSERT INTO tb_offer(edition, start_moment, end_moment, course_id) VALUES('1.0', TIMESTAMP WITH TIME ZONE '2021-07-13T20:50:07.12345Z', TIMESTAMP WITH TIME ZONE '2022-01-04T09:50:07.12345Z', 1);
+
+INSERT INTO tb_notification(text, moment, read, route, user_id) VALUES('NOTIFICAÇÃO DE COMPRA DO BOOTCAMP', TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07.12345Z', true, '/notification', 1);
